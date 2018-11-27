27257f9db8331454de25aa808a7a8b2f4bc84549
Broivula <elias.koivula1@hotmail.com>
2018-11-27 13:50:58 +0200

    main
    started on passport, module not yet created

--- index.js
+++ index.js
@@ -2,13 +2,57 @@
 
 console.log("Hello World!");
 
-require('dotenv').config()
-
+require('dotenv').config();
+const http = require('http');
+const https = require('https');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 const express = require('express');
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const fs = require('fs');
 const app = express();
-var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' });
+
+const sslkey = fs.readFileSync('/etc/pki/tls/private/ca.key');
+const sslcrt = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
+const options ={
+    key: sslkey,
+    cert: sslcrt,
+};
+
+
+app.use(bodyParser.urlencoded({extend: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
 //const mysql = require('mysql2');
+
+passport.serializeUser((user, done) =>{
+   console.log('serializing user: ' + user.username);
+   done(null, user.id);
+});
+
+passport.deserializeUser((user, done) =>{
+   done(null, user);
+});
+
+app.use(session({
+  secret: 'placeholder',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {secure:true},
+}));
+
+passport.use(new LocalStrategy(
+    (username, password, done) =>{
+        if(username != process.env.USER || password != process.env.PASS){return done(null, false);}
+        return done(null, {username: username});
+    }
+));
 
 /*const connection = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -19,7 +63,9 @@ var upload = multer({ dest: 'uploads/' });
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+app.post('/login', passport.authenticate('local', {successRedirect: '/fotemahome', failureRedirect: '/login'}));
+
+app.get('/', (req, res) =>{
     /*connection.query('select * from employee', (err, results, fields) => {
         if (err) res.send('DB error.');
         res.send(results);
@@ -27,14 +73,20 @@ app.get('/', function (req, res) {
     res.send('Hello world');
 })
 
-app.get('/uploads/:name', function (req, res) {
+app.get('/uploads/:name', (req, res)  =>{
     res.sendFile('uploads/' + req.params.name, { root: __dirname });
 })
 
 
-app.post('/imgupload', upload.single('imagefile'), function (req, res, next) {
+app.post('/imgupload', upload.single('imagefile'),  (req, res, next) =>{
     var path = req.file.path;
     res.send(`File uploaded in ${path}`);
 });
 
-app.listen(3000);
+const proxy = http.createServer((req, res)=>{
+    console.log('creating http server on port: 8000');
+    res.writeHead(302, {'Location': 'https://' + req.headers.host + '/node/' + req.url});
+    res.end();
+}).listen(8000);
+
+https.createServer(options, app).listen(3000);
