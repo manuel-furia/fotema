69db0ba5d73e4eac8baa3a5aee0db9a33be8f127
Broivula <elias.koivula1@hotmail.com>
2018-11-29 15:41:00 +0200

    views folder
    added the base for the views folder, read the readme inside of it for more information.

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ const LocalStrategy = require('passport-local').Strategy;
 const sslkey = fs.readFileSync('/etc/pki/tls/private/ca.key');
 const sslcrt = fs.readFileSync('/etc/pki/tls/certs/ca.crt');
 //const resize = resizeMod.resize;
+//const mysql = require('mysql2');
+
+// tell where the static files are located.
+app.use(express.static(__dirname + '/view'));
+
+
 
 const options ={
   key: sslkey,
@@ -30,6 +36,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.urlencoded({extend: true}));
 
+//passport initialization
 passport.use(passportMod.loginStrategy);
 passport.serializeUser(passportMod.serializeUser);
 passport.deserializeUser(passportMod.deserializeUser);
@@ -42,13 +49,6 @@ app.use(session({
 }));
 
 
-// - - - - start a new 'local strategy' for user trying to log in. basically just checking if the username and password are correct.
-// - - - -  NOTE! the username and password have to be later queryd from the database. the current thing is for testing purposes.
-
-//const mysql = require('mysql2');
-
-
-
 /*const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -56,18 +56,26 @@ app.use(session({
     database: process.env.DB_DATABASE
 });*/
 
+
 app.use(express.static('public'));
 
-// - - - current login page. starting the authentication process.
+// - - - gets the login post. starting the authentication process.
 app.post('/login',
-  passport.authenticate('local', {successRedirect: '/node/', failureRedirect: '/loginfailedpage'}));
+  passport.authenticate('local', {successRedirect: '/node/', failureRedirect: '/loginfailedpage'})
+);
 
-app.get('/', (req, res) =>{
-  /*connection.query('select * from employee', (err, results, fields) => {
-      if (err) res.send('DB error.');
-      res.send(results);
-  });*/
-  res.send('Hello world');
+//homepage!
+app.get('/', (req, res, next) =>{
+    //first we should check if the user is signed in or not, since that is how we present the context.
+    //after knowing if the user is signed in or not, we give them the frontpage.
+    // --> if signed in user gets modified front page
+    // --> if not, then gets the normal frontpage.
+  if(req.user){                                         //--> user is signed in, show the custom homepage
+
+  }else{
+                                                        //--> user is not logged in, show basic homepage
+  }
+  res.render('index');
 });
 
 app.get('/uploads/:name', (req, res)  =>{
---
+++ views/readme.txt
@@ -0,0 +1,11 @@
+    -   -   -   -   - VIEWS -   -   -   -   -
+
+ This directory holds the html, that is going to be handled by the UI module, before
+ passing it as a html.
+
+ All of these files are the basic html files, that have special <$> tags inserted inside of them.
+ Inside of these tags, the UI module is going to create the dynamic content.
+
+
+ Files that are done:  (update as more gets done)
+ clickedmedia.html
